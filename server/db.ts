0e853c6a92a700a10c77ecc48b49c267953cae88
Hemant Raj <coder.hemantraj05@gmail.com>
2025-02-12 00:43:09 +0530

    Added database so that it can work locally

--- server/db.ts
+++ server/db.ts
@@ -2,6 +2,7 @@ import { Pool, neonConfig } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-serverless';
 import ws from "ws";
 import * as schema from "@shared/schema";
+import "dotenv/config";
 
 neonConfig.webSocketConstructor = ws;
 
@@ -11,5 +12,10 @@ if (!process.env.DATABASE_URL) {
   );
 }
 
-export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const connectionString = `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}/${process.env.PGDATABASE}?sslmode=require`;
+const pool = new Pool({
+  connectionString,
+  max: 10, // Optional: Limit the number of connections
+});
+
 export const db = drizzle({ client: pool, schema });
